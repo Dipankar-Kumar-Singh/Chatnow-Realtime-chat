f18e717eef1a2a1b083b8bd56cc93ee7f457ac6e
Dipankar <dipankarcode@gmail.com>
2023-05-05 19:32:01 +0530

    making object

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 const express = require('express');
-const path = require('path') ;
-const http = require('http') ;
-const socketio = require('socket.io') ;
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+const formateMesssage = require('./utils/messages');
 
-// 
-const app = express() ;
-const server = http.createServer(app)
-const PORT = 3000 || process.env.PORT ;
-const io = socketio(server) ;
+//
+const app = express();
+const server = http.createServer(app);
+const PORT = 3000 || process.env.PORT;
+const io = socketio(server);
 
-app.use(express.static(path.join(__dirname ,  'public'))) ;
+const botName = 'Chatnow Bot';
 
-io.on('connection' , socket => {
-    console.log("New Connection .... ") ;
-    socket.emit("messageID" , "fgdfdf") ;
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // Broadbase when a user disconect 
+// Runs when client connects
+io.on('connection', (socket) => {
+	console.log('New Connection Register.... ');
 
-})
+	// wlecome current user
+	socket.emit(
+		'message',
+		formateMesssage(botName, 'Welcome to Chat Room.....')
+	);
 
+	// Broadbase when a user disconect
+	socket.broadcast.emit(
+		'message',
+		formateMesssage(botName, 'A user has joined the Chat')
+	);
 
-server.listen(PORT , ()=>{
-    console.log(`Server running on port ${PORT}`) 
-})
+	// Runs when client Disconneccts
+	socket.on('disconnect', () => {
+		io.emit(
+			'message',
+			formateMesssage(botName, 'A user has Left the Chat')
+		);
+	});
+
+	// Listen for chatMessage
+	socket.on('chatMessage', (message) => {
+		// console.log(message) ;
+		io.emit('message', formateMesssage('USER', message));
+	});
+});
+
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,46 @@
-const socket = io() ;
+const chatForm = document.getElementById('chat-form');
 
-socket.on('messageID' , message => {
-    console.log(message) ;
-})
+const socket = io();
+const chatMessages = document.querySelector('.chat-messages');
 
+
+// Message from server
+socket.on('message', (message) => {
+	console.log(message);
+	displayMessage(message);
+
+    // scroll down 
+    chatMessages.scrollTop = chatMessages.scrollHeight ;
+
+
+});
+
+chatForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	// get message text
+	const msg = e.target.elements.msg.value;
+
+	// Emit message to server
+	socket.emit('chatMessage', msg);
+
+    // Clear input 
+    e.target.elements.msg.value = '' ;
+    e.target.elements.msg.focus() ;
+
+
+});
+
+// Output message to DOM
+function displayMessage(message) {
+	const div = document.createElement('div');
+	div.classList.add('message');
+	div.innerHTML = `
+        <p class="meta">Mary <span>9:15pm</span></p>
+        <p class="text">
+            ${message}
+        </p>
+    `;
+
+    document.querySelector('.chat-messages').appendChild(div) ;
+}
