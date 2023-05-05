56502c5facb9e5c549fa612a1115c998ef26afe1
Dipankar <dipankarcode@gmail.com>
2023-05-05 15:58:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path') ;
+const http = require('http') ;
+const socketio = require('socket.io') ;
+
+// 
+const app = express() ;
+const server = http.createServer(app)
+const PORT = 3000 || process.env.PORT ;
+const io = socketio(server) ;
+
+app.use(express.static(path.join(__dirname ,  'public'))) ;
+
+io.on('connection' , socket => {
+    console.log("New Connection .... ") ;
+    socket.emit("messageID" , "fgdfdf") ;
+
+
+
+    // Broadbase when a user disconect 
+
+})
+
+
+server.listen(PORT , ()=>{
+    console.log(`Server running on port ${PORT}`) 
+})
---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+const socket = io() ;
+socket.on('messageID' , message => {
+    console.log(message) ;
+})
